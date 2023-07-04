96142e582f6108588e2f96de896d90e962fda127
KitsuneAlex <alexh@karmakrafts.dev>
2023-07-04 22:16:54 +0200

    Add kstd-reflect.cmake module

---
+++ kstd-reflect.cmake
@@ -0,0 +1,11 @@
+set(KSTD_REFLECT_VERSION master)
+
+macro(target_include_kstd_reflect target)
+    FetchContent_Declare(
+        kstd-reflect
+        GIT_REPOSITORY https://github.com/karmakrafts/kstd-reflect.git
+        GIT_TAG "${KSTD_REFLECT_VERSION}"
+    )
+    FetchContent_MakeAvailable(kstd-reflect)
+    target_include_directories(${target} PUBLIC "${CMAKE_CURRENT_BINARY_DIR}/_deps/kstd-reflect-src/include")
+endmacro()

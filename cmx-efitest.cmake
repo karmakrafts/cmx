793de624250a086a4716f606eb6f7399b664e835
KitsuneAlex <alexh@karmakrafts.dev>
2023-09-26 00:46:36 +0200

    Overwrite EFITEST_SOURCE_DIR when included from cmx-efitest module

--- cmx-efitest.cmake
+++ cmx-efitest.cmake
@@ -2,7 +2,7 @@ include_guard()
 
 set(CMX_EFITEST_VERSION master)
 set(CMX_EFITEST_FETCHED OFF)
-    
+
 if(NOT CMX_EFITEST_FETCHED)
     FetchContent_Declare(
         efitest
@@ -10,5 +10,6 @@ if(NOT CMX_EFITEST_FETCHED)
         GIT_TAG ${CMX_EFITEST_VERSION}
     )
     FetchContent_MakeAvailable(efitest)
+    set(EFITEST_SOURCE_DIR "${efitest_SOURCE_DIR}")
     set(CMX_EFITEST_FETCHED ON)
 endif() # CMX_EFITEST_FETCHED

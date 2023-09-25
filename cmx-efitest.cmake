60614ae7c56a9fa32de11dacb8e64b4009a9b5c4
KitsuneAlex <alexh@karmakrafts.dev>
2023-09-25 23:15:27 +0200

    Always include module path of efitest

--- cmx-efitest.cmake
+++ cmx-efitest.cmake
@@ -3,21 +3,13 @@ include_guard()
 set(CMX_EFITEST_VERSION master)
 set(CMX_EFITEST_FETCHED OFF)
     
-macro(cmx_include_efitest target)
-    set(num_args ${ARGC})
-    if(num_args GREATER 1)
-        set(access ${ARGV1}) # Copy first optional argument
-    else()
-        set(access PUBLIC) # Default to PUBLIC
-    endif()
-    if(NOT CMX_EFITEST_FETCHED)
-        FetchContent_Declare(
-            efitest
-            GIT_REPOSITORY https://github.com/kos-project/libefitest.git
-            GIT_TAG ${CMX_EFITEST_VERSION}
-        )
-        FetchContent_MakeAvailable(efitest)
-        set(CMX_EFITEST_FETCHED ON)
-    endif() # CMX_EFITEST_FETCHED
-    list(APPEND CMAKE_MODULE_PATH "${efitest_SOURCE_DIR}/cmake")
-endmacro()
+if(NOT CMX_EFITEST_FETCHED)
+    FetchContent_Declare(
+        efitest
+        GIT_REPOSITORY https://github.com/kos-project/libefitest.git
+        GIT_TAG ${CMX_EFITEST_VERSION}
+    )
+    FetchContent_MakeAvailable(efitest)
+    set(CMX_EFITEST_FETCHED ON)
+endif() # CMX_EFITEST_FETCHED
+list(APPEND CMAKE_MODULE_PATH "${efitest_SOURCE_DIR}/cmake")

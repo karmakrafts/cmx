1d4facfce25ed5c0d17ad952ab55cc4b2445eda0
KitsuneAlex <alexh@karmakrafts.dev>
2023-09-29 03:11:56 +0200

    Include EFITEST module when included through CMX module

--- cmx-efitest.cmake
+++ cmx-efitest.cmake
@@ -12,5 +12,7 @@ if(NOT CMX_EFITEST_FETCHED)
     FetchContent_MakeAvailable(efitest)
     set(EFITEST_SOURCE_DIR "${efitest_SOURCE_DIR}")
     set(EFITEST_BINARY_DIR "${efitest_BINARY_DIR}")
+    list(APPEND CMAKE_MODULE_PATH "${efitest_SOURCE_DIR}/cmake")
+    include(efitest)
     set(CMX_EFITEST_FETCHED ON)
 endif() # CMX_EFITEST_FETCHED
